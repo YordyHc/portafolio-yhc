a21b0ccdfab8a4a513a97e8f9ae9f6693ad3eb31
YordyHc <yordyhuamanc@gmail.com>
2026-07-28 02:36:09 -0500

    otro

--- docker/start.sh
+++ docker/start.sh
@@ -14,11 +14,6 @@ echo "Optimizando Laravel..."
 
 php artisan optimize:clear
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-
 echo "Aplicación lista"
 
 echo "Probando conexión SMTP..."
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,13 @@ Route::get('/test', function () {
     ]);
 });
 Route::post('/clientes', [ContactoController::class, 'store']);
+
+Route::get('/debug-mail', function () {
+    return response()->json([
+        'mailer' => config('mail.default'),
+        'host' => config('mail.mailers.smtp.host'),
+        'port' => config('mail.mailers.smtp.port'),
+        'encryption' => config('mail.mailers.smtp.encryption'),
+        'username' => config('mail.mailers.smtp.username'),
+    ]);
+});
