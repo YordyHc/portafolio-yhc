f1af94507730311127405f6f262cbf77c0c6e155
YordyHc <yordyhuamanc@gmail.com>
2026-07-28 19:06:50 -0500

    Refactor: cambiar el flujo de envio de correos usando api OAuth de fmail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleAuthController;
+
+
+Route::get('/oauth/gmail', [
+    GoogleAuthController::class,
+    'redirect'
+]);
+
+
+Route::get('/oauth/gmail/callback', [
+    GoogleAuthController::class,
+    'callback'
+]);
+
 
 Route::get('/', function () {
     return view('welcome');
